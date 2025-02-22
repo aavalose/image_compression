b0fd0bb7fcb9f38bc994b33fb9acd3829d09e50a
khushibhat1999 <138834532+khushibhat1999@users.noreply.github.com>
2025-02-21 19:30:06 -0800

    Add files via upload

--- msds697_group2_aggregate.js
+++ msds697_group2_aggregate.js
@@ -47,3 +47,8 @@ db.raw_data.aggregate([
     "$out": "image_sized"
   }
 ])
+
+//queries
+
+db["image_sized"].find().count() //output: 15
+db["image_sized"].find().sort({'dimension_x' : -1}) //sorts the documents by 'dimension_x' in descending order
