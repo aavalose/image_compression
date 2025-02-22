17fc537af7fa04c858ad3de035ff2b3bc672c5fb
khushibhat1999 <138834532+khushibhat1999@users.noreply.github.com>
2025-02-21 19:01:56 -0800

    Add files via upload

---
+++ msds697_group2_aggregate.js
@@ -0,0 +1,49 @@
+db.raw_data.aggregate([
+  {
+    "$project": {
+      "id": 1,
+      "url": 1,
+      "short_url": 1,
+      "views": 1,
+      "favorites": 1,
+      "source": 1,
+      "purity": 1,
+      "category": 1,
+      "dimension_x": { "$ifNull": ["$dimension_x", "$width"] },
+      "dimension_y": { "$ifNull": ["$dimension_y", "$height"] },
+      "resolution": 1,
+      "ratio": 1,
+      "file_size": 1,
+      "file_type": 1,
+      "created_at": 1,
+      "colors": 1,
+      "path": 1,
+      "thumbs": 1,
+      "area": {
+        "$multiply": [
+          { "$ifNull": ["$dimension_x", "$width"] },
+          { "$ifNull": ["$dimension_y", "$height"] }
+        ]
+      }
+    }
+  },
+  {
+    "$addFields": {
+      "photo_size": {
+        "$cond": {
+          "if": { "$gt": ["$area", 480000] },
+          "then": "medium",
+          "else": "small"
+        }
+      }
+    }
+  },
+  {
+    "$project": {
+      "area": 0
+    }
+  },
+  {
+    "$out": "image_sized"
+  }
+])
